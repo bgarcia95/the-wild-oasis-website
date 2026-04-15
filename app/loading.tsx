c88e91a8b1b3b2df32b0121b57cd9a54f1e7dce9
Brayan Garcia <bgarcia.95@outlook.com>
2026-04-15 15:43:02 -0600

    feat: Add Logo and Navigation components; implement data service functions; create loading state; update global styles; add images

---
+++ app/loading.tsx
@@ -0,0 +1,4 @@
+// This will work for all loading states in the app, but you can customize it to be more specific if you want.
+export default function Loading() {
+  return <p>LOADING DATA...</p>;
+}
