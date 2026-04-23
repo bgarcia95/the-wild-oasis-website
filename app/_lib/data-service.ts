68fe59d56f734bec48a02778c131d4338eba3f8a
Brayan Garcia <bgarcia.95@outlook.com>
2026-04-22 18:47:18 -0600

    feat: Add NotFound component for cabin and page not found scenarios

--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -1,6 +1,7 @@
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
 import { Booking, Guest } from "../_types/types";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -17,6 +18,7 @@ export async function getCabin(id: number) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
@@ -41,6 +43,9 @@ export const getCabins = async function () {
     .from("cabins")
     .select("id, name, maxCapacity, regularPrice, discount, image")
     .order("name");
+
+  // For testing
+  // await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
     console.error(error);
